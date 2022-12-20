8316324cbf3dd64f8a6546079159adc1ac16d22e
gahyeon11 <117976216+gahyeon11@users.noreply.github.com>
2022-12-20 18:22:30 +0900

    기울기 볼륨
    아마 최종

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
-let permission = false; //허가를 위한 변수 (기본은 허가 안남)
+let permission = false; 
 let osc;
 var wave = [];
-
 let button;
 
 
@@ -10,12 +9,13 @@ let button;
 function setup() {
   createCanvas(500, 500);
   if( typeof DeviceMotionEvent.requestPermission == "function"){
-    background(255, 0, 0);
+    background(255,255,153);
     button = createButton("Click to ios sensor")
     button.mousePressed(iosAccess); 
   }else{
-    background(0, 255, 0);
-    text ("is not a ios",100,100);
+    background(153,153,255);
+    textSize(72);
+    text ("is not a ios",70,200);
 
   }
   
@@ -63,9 +63,11 @@ function draw() {
   fill(051,051,153);
   rect(400,250,100,250);
 
+  // textSize(72);
+  // text(rotationX/100, 100, 100);
 }
 function touchStarted() {
-  getAudioContext().resume(); //강제로 사운드 허용하는 문법(구글 및 애플 뚫기)
+  getAudioContext().resume();
 
 for (var i = 0; i < touches.length; i++){ 
 
@@ -73,58 +75,58 @@ for (var i = 0; i < touches.length; i++){
   {if(touches[i].x>0 && touches[i].x<100 ){
       wave[0].freq(261);
       wave[0].start();
-      wave[0].amp(0.5, 1);
+      wave[0].amp(rotationX/100, 1);
       wave[0].amp(0,1);
   }
   if(touches[i].x>101 && touches[i].x<200 ){
       wave[1].freq(293);
       wave[1].start();
-     wave[1].amp(0.5, 1);
+     wave[1].amp(rotationX/100, 1);
       wave[1].amp(0,1);
    }    
   if(touches[i].x>201 && touches[i].x<300 ){
       wave[2].freq(329);
       wave[2].start();
-      wave[2].amp(0.5, 1);
+      wave[2].amp(rotationX/100, 1);
       wave[2].amp(0,1);
 
    }else if(touches[i].x>301 && touches[i].x<400 ){
       wave[3].freq(349);
       wave[3].start();
-      wave[3].amp(0.5, 1);
+      wave[3].amp(rotationX/100, 1);
       wave[3].amp(0,1);
    }else if(touches[i].x>401 && touches[i].x<500 ){
       wave[4].freq(391);
       wave[4].start();
-      wave[4].amp(0.5, 1);
+      wave[4].amp(rotationX/100, 1);
       wave[4].amp(0,1);
    }
   }else
   {if(touches[i].x>0 && touches[i].x<100  ){
       wave[5].freq(523);
       wave[5].start();
-      wave[5].amp(0.5, 1);
+      wave[5].amp(rotationX/100, 1);
       wave[5].amp(0,1);
    }else if(touches[i].x>101 && touches[i].x<200  ){
       wave[6].freq(587);
       wave[6].start();
-      wave[6].amp(0.5, 1);
+      wave[6].amp(rotationX/100, 1);
       wave[6].amp(0,1);
    } if(touches[i].x>201 && touches[i].x<300 ){
       wave[7].freq(659);
       wave[7].start();
-      wave[7].amp(0.5, 1);
+      wave[7].amp(rotationX/100, 1);
       wave[7].amp(0,1);
 
    }else if(touches[i].x>301 && touches[i].x<400 ){
       wave[8].freq(698);
       wave[8].start();
-      wave[8].amp(0.5, 1);
+      wave[8].amp(rotationX/100, 1);
       wave[8].amp(0,1);
    }else if(touches[i].x>401 && touches[i].x<500 ){
       wave[9].freq(783);
       wave[9].start();
-      wave[9].amp(0.5, 1);
+      wave[9].amp(rotationX/100, 1);
       wave[9].amp(0,1);
    }
   
